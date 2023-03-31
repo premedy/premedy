a6b3b71c80319b4fd3a9ea682b39d7cad8698229
Fernando Hernandez <fernando.hernandez@premise.com>
2023-03-30 19:04:15 -0600

    feat: new test for RemediationBase

--- premedy/tests/test_remediation_base.py
+++ premedy/tests/test_remediation_base.py
@@ -4,7 +4,7 @@ from premedy.remediation_base import RemediationBase
 
 
 class TestRemediationBase(unittest.TestCase):
-    def test_ini_requires_to_be_executed_by_child_class(self):
+    def test_init_requires_to_be_executed_by_child_class(self):
         finding_result = None
 
         with self.assertRaises(Exception) as e:
@@ -14,3 +14,13 @@ class TestRemediationBase(unittest.TestCase):
             e.exception.args[0],
             "missing category: set a category in the child instance",
         )
+
+    def test_init_with_child_class(self):
+        finding_result = None
+
+        class ChildRemediation(RemediationBase):
+            category = "CATEGORY"
+
+        remediation = ChildRemediation(finding_result)
+
+        self.assertEqual(remediation.category, "CATEGORY")
